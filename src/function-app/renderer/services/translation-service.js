be75b13b777bbcf0577df3e9ed66ee9a964023c2
Anton Boyko <boyko.ant@live.com>
2017-02-13 00:46:11 +0200

    started with hardcoded translation service

---
+++ src/function-app/renderer/services/translation-service.js
@@ -0,0 +1,21 @@
+var service = {};
+
+service.getMonthName = function(n) {
+	switch (n) {
+		case 0: return 'января';
+		case 1: return 'февраля';
+		case 2: return 'марта';
+		case 3: return 'апреля';
+		case 4: return 'мая';
+		case 5: return 'июня';
+		case 6: return 'июля';
+		case 7: return 'августа';
+		case 8: return 'сентября';
+		case 9: return 'октября';
+		case 10: return 'ноября';
+		case 11: return 'декабря';
+	}
+	throw new RangeError();
+};
+
+module.exports = service;
