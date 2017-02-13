84aeee52678444bde57f13763a3bc78419b4332e
Boyko Anton <boyko.ant@gmail.com>
2017-02-13 12:35:41 +0200

    added cdn endpoint to assets service

--- src/function-app/renderer/services/assets-service.js
+++ src/function-app/renderer/services/assets-service.js
@@ -1,38 +1,39 @@
 var _ = require('lodash');
 
-var assets = function() {
+var assets = function(cdnEndpoint) {
+	this.cdnEndpoint = cdnEndpoint || '';
 	this.css = [];
 	this.js = [];
 };
 
 assets.prototype.addJQuery = function() {
-	this.js.push('/assets/jquery/jquery-3.1.1.min.js');
+	this.js.push(cdnEndpoint + '/assets/jquery/jquery-3.1.1.min.js');
 	return this;
 };
 
 assets.prototype.addJQueryUI = function() {
 	this.addJQuery();
-	this.css.push('/assets/jquery-ui/jquery-ui.min.css');
-	this.css.push('/assets/jquery-ui/jquery-ui.structure.min.css');
-	this.js.push('/assets/jquery-ui/jquery-ui.min.js');
+	this.css.push(cdnEndpoint + '/assets/jquery-ui/jquery-ui.min.css');
+	this.css.push(cdnEndpoint + '/assets/jquery-ui/jquery-ui.structure.min.css');
+	this.js.push(cdnEndpoint + '/assets/jquery-ui/jquery-ui.min.js');
 	return this;
 };
 
 assets.prototype.addBootstrap = function() {
 	this.addJQuery();
-	this.css.push('/assets/bootstrap/css/bootstrap.min.css');
-	this.css.push('/assets/bootstrap/css/bootstrap-theme.min.css');
-	this.js.push('/assets/bootstrap/js/bootstrap.min.js');
+	this.css.push(cdnEndpoint + '/assets/bootstrap/css/bootstrap.min.css');
+	this.css.push(cdnEndpoint + '/assets/bootstrap/css/bootstrap-theme.min.css');
+	this.js.push(cdnEndpoint + '/assets/bootstrap/js/bootstrap.min.js');
 	return this;
 };
 
 assets.prototype.addFontAwesome = function() {
-	this.css.push('/assets/font-awesome/css/font-awesome.min.css');
+	this.css.push(cdnEndpoint + '/assets/font-awesome/css/font-awesome.min.css');
 	return this;
 };
 
 assets.prototype.addKnockout = function() {
-    this.js.push('/assets/knockout/knockout-3.4.1.js');
+    this.js.push(cdnEndpoint + '/assets/knockout/knockout-3.4.1.js');
     return this;
 };
 
@@ -45,8 +46,8 @@ assets.prototype.render = function() {
 
 var service = {};
 
-service.create = function() {
-	return new assets();
+service.create = function(cdnEndpoint) {
+	return new assets(cdnEndpoint);
 };
 
 module.exports = service;
